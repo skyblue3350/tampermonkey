88eac58fe6d806a25a54c34420197682c6a04e54
sky <skyblue3350@gmail.com>
2024-10-13 22:51:57 +0900

    Update amazon-prime-video-disable-nextup.user.js

--- amazon-prime-video-disable-nextup.user.js
+++ amazon-prime-video-disable-nextup.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Amazon Prime VideoのNext Upを非表示にするユーザースクリプト
 // @namespace    skyblue3350
-// @version      1.0.0
+// @version      1.0.1
 // @description  次の回へ誘導するポップアップを非表示にします
 // @author       skyblue3350
 // @match        https://www.amazon.co.jp/gp/video/detail*
@@ -13,9 +13,6 @@
 
     setTimeout(() => {
         const nextCard = Array.from(document.querySelectorAll("div")).find(item => item.className.includes("nextupcard-wrapper"))
-        console.log(Array.from(document.querySelectorAll("div")).map(item => item.className))
-        console.log(nextCard)
         nextCard.style.visibility = "hidden"
-        console.log(nextCard.style.visibility)
     }, 3000)
 })();
